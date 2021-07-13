ac6fe20630fb523bfd83d48258cdd25d21979866
Akshat Rawat <akshatrwt00@gmail.com>
2021-07-13 17:25:57 +0530

    test1

--- collateral-web-portal/src/main/java/com/cts/training/collateralwebportal/controller/HomeController.java
+++ collateral-web-portal/src/main/java/com/cts/training/collateralwebportal/controller/HomeController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class HomeController {
 
