ab7ae26e0ca74c5a299abe1b0566e18056e02512
Yash Kapoor <kapooryash713@gmail.com>
2021-07-14 23:27:43 +0530

    rectified authorization

--- authorization-service/src/main/java/com/cognizant/AuthServiceApplication.java
+++ authorization-service/src/main/java/com/cognizant/AuthServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * This is the main class for Authorisation class
  * 
- * @author rawat
+ * @author 
  *
  */
 @SpringBootApplication
